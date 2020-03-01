653cb9d2ec104f2251b1fcc1a12568c9bc8969fb
Dinesh Bhusal <din520818@gmail.com>
2020-03-01 11:44:29 +0545

    Flask API Created.

---
+++ apiServer.py
@@ -0,0 +1,30 @@
+from os import path
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Welcome(Resource):
+    def get(self):
+        return {"key": "Welcome to Project X"}
+
+
+class firstPost(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("arg", type=str, default="It works")
+        args = parser.parse_args()
+        arg = args['arg']
+        print(arg)
+
+
+# welcome page
+api.add_resource(Welcome, "/")
+
+api.add_resource(firstPost, "/firstPost")
+
+if __name__ == "__main__":
+    absPath = path.abspath(__file__)
+    app.run(debug=True, port=4000, host="localhost")
