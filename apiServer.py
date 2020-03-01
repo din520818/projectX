31a2c938b3bbd172b640c24fca643b0f05e291cd
Dinesh Bhusal <din520818@gmail.com>
2020-03-01 14:07:28 +0545

    CORS enabled

--- apiServer.py
+++ apiServer.py
@@ -1,8 +1,10 @@
 from os import path
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
